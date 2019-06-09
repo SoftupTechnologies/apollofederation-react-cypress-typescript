85d96a04ee81ef92fca1f93fbc38ed48299416f6
Shpetim Selaci <shselaci3@gmail.com>
2019-06-09 18:36:35 +0200

    re-added app

---
+++ app/src/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Application from './App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import * as serviceWorker from './serviceWorker';
+
+const App = async () => {
+    const client = await new ApolloClient({
+        uri: 'localhost:4000'
+    });
+    return (
+        <ApolloProvider client={client}>
+            <Application />
+        </ApolloProvider>
+    )
+}
+
+const renderApp = async () => {
+    ReactDOM.render(await App(), document.getElementById('root'));
+}
+renderApp();
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
