f8f41ab3a94e479108652df941b9e45d254dc446
Shpetim Selaci <shselaci3@gmail.com>
2019-06-17 01:59:42 +0200

    finalized tests

--- e2e/cypress/integration/add_timelog.spec.js
+++ e2e/cypress/integration/add_timelog.spec.js
@@ -8,8 +8,11 @@ describe('creates a timelog and checks if it has been added succesfully to our t
     cy.get('.author > .bp3-button').click();
     cy.get('.bp3-input-group > .bp3-input').type(8);
     cy.get('.bp3-fill').type('This is a test log!!');
-    cy.get('.bp3-dialog-footer-actions > .bp3-button').click().should('have.class', 'bp3-loading');
+    cy.get('.bp3-dialog-footer-actions > .bp3-button').click();
     cy.get('.bp3-card:last-child > div').should('have.text', 'This is a test log!!');
     cy.get('.bp3-card:last-child > p').should('have.text', 'Total: 8h');
+    cy.get('.bp3-card:last-child').scrollIntoView({
+      easing: "linear",
+    })
   })
 })
